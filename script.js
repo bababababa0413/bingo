7ba31ffc7599ba2de87a3e7b8638385b11fe4b09
baba <win501tsubasa501baba@outlook.com>
2025-01-29 10:23:03 +0900

    値の重複の有無を選択できるように変更

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', function () {
     const deleteButtons = document.querySelectorAll('.delete');
 
-    for(let i = 0; i < deleteButtons.length; i++){
+    for (let i = 0; i < deleteButtons.length; i++) {
         // 参加者の削除ボタンが押されたときの処理
-        deleteButtons[i].addEventListener('click', function(e){
+        deleteButtons[i].addEventListener('click', function (e) {
             const numberOfButtons = document.querySelectorAll('.delete').length;
-            if(numberOfButtons !== 1){
+            if (numberOfButtons !== 1) {
                 e.target.parentElement.remove();
             }
         });
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const plusButton = document.getElementById('plus');
 
     // 参加者追加ボタンが押されたときの処理
-    plusButton.addEventListener('click', function(e){
+    plusButton.addEventListener('click', function (e) {
         e.target.remove();
 
         const participantsTd = document.getElementById('participants');
@@ -23,9 +23,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
         div.querySelector('.participant').value = '';
         const button = div.querySelector('.delete');
-        button.addEventListener('click', function(e) {
+        button.addEventListener('click', function (e) {
             const numberOfButtons = document.querySelectorAll('.delete').length;
-            if(numberOfButtons !== 1){
+            if (numberOfButtons !== 1) {
                 e.target.parentElement.remove();
             }
         });
@@ -43,8 +43,17 @@ window.addEventListener('DOMContentLoaded', function () {
         }
 
         const size = parseInt(document.getElementById('size').value);
+
         const rangeMin = parseInt(document.getElementById('range-min').value);
+
         const rangeMax = parseInt(document.getElementById('range-max').value);
+
+        const duplicateValue = document.querySelector('input[name="duplicate-column"]:checked').value;
+        let permitDuplicate = true;
+        if (duplicateValue === 'no-duplicate') {
+            permitDuplicate = false;
+        }
+
         const participantsElements = document.querySelectorAll('.participant');
         const participants = Array.from(participantsElements).map(participantsElements => participantsElements.value.trim());
 
@@ -65,18 +74,18 @@ window.addEventListener('DOMContentLoaded', function () {
             alert('点数の範囲が不正です。');
             return;
         }
-        if (rangeMax - rangeMin + 1 < size * size) {
+        if (rangeMax - rangeMin + 1 < size * size && permitDuplicate === false) {
             alert('設定した点数の範囲では、埋めれないセルがあります。');
             return;
         }
-        if(participants.includes('')){
+        if (participants.includes('')) {
             alert('参加者を設定してください。');
             return;
-        }       
+        }
 
         // ビンゴシート生成
         const table = document.getElementById('bingo');
-        createBingoSheet(table, size, rangeMin, rangeMax, participants);
+        createBingoSheet(table, size, rangeMin, rangeMax, permitDuplicate, participants);
 
     });
 
@@ -87,15 +96,16 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param {*} min シートの値の下限値
      * @param {*} max シートの値の上限値
      */
-    function createBingoSheet(table, size, min, max, participants) {
+    function createBingoSheet(table, size, min, max, permitDuplicate, participants) {
         // 既存のシートを削除
         for (let i = table.children.length - 1; i >= 0; i--) {
             table.children[i].remove();
         }
 
-        // シートに入れる値をSetで受け取る
-        const randomSet = createRandomSet(size, min, max);
-        const randomSetIterator = randomSet.values();
+        // シートに入れる値を配列で受け取る
+        const randomNumbers = createRandomArray(size, min, max, permitDuplicate);
+        // イテレータ作成
+        const randomNumbersIterator = randomNumbers[Symbol.iterator]();
 
         // 指定されたサイズのビンゴシートを生成 
         for (let i = 0; i < size; i++) {
@@ -108,7 +118,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
                 // 数字部分
                 const span = document.createElement('span');
-                span.innerText = randomSetIterator.next().value;
+                span.innerText = randomNumbersIterator.next().value;
                 div.append(span);
 
                 // あけた名前入力部分を作成
@@ -123,23 +133,33 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     /**
-     * ビンゴシートの大きさ分の乱数を生成し、Setで返す関数
+     * ビンゴシートの大きさ分の乱数を生成し、配列で返す関数
      * @param {*} size ビンゴシートのサイズ 
      * @param {*} min 乱数の下限値
      * @param {*} max 乱数の上限値
-     * @returns ビンゴシートに入れる値を格納したSet
+     * @param {*} permitDuplicate 重複を許可するか(true/false)
+     * @returns ビンゴシートに入れる値を格納した配列
      */
-    function createRandomSet(size, min, max) {
-        // ビンゴシートの値は重複を許さないので、Setを使う
-        randomSet = new Set();
+    function createRandomArray(size, min, max, permitDuplicate) {
+        if (permitDuplicate) {
+            const randomArray = new Array();
 
-        // Setの要素数がビンゴシートを埋め切れる数になるまで乱数生成
+            while (randomArray.length < size * size) {
+                const num = Math.floor(Math.random() * (max - min + 1)) + min;
+                randomArray.push(num);
+            }
+
+            return randomArray;
+        }
+
+        const randomSet = new Set();
+
         while (randomSet.size < size * size) {
             const num = Math.floor(Math.random() * (max - min + 1)) + min;
             randomSet.add(num);
         }
 
-        return randomSet;
+        return Array.from(randomSet);
     }
 
     /**     
@@ -147,20 +167,20 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param {*} participants 参加者の名前が入った配列
      * @returns あけた人の名前入力を選択するプルダウンリスト
      */
-    function createParticipantsSelect(participants){
+    function createParticipantsSelect(participants) {
         // あけた人の名前入力部分
         const participantsSelect = document.createElement('select');
         participantsSelect.class = 'participants-select';
-        
+
         const option = document.createElement('option');
         option.value = '';
 
         participantsSelect.append(option);
 
-        for(let k = 0; k < participants.length; k++){
+        for (let i = 0; i < participants.length; i++) {
             const option = document.createElement('option');
-            option.innerText = participants[k];
-            option.value = k;
+            option.innerText = participants[i];
+            option.value = i;
             participantsSelect.append(option);
         }
 
